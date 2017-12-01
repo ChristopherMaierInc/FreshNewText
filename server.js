6b11f8b92cb753d7cea922dbcd6d82e6cf32c0e9
Christopher <mail@christophermaier.com.au>
2017-12-01 16:39:47 +1000

    Backend dev: Add logging to the server so you can watch requests come through + debug. Don't forget to create documentation for your API in the README.

--- server.js
+++ server.js
@@ -1,24 +1,56 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const messages = [
   {
     id: 1,
     author: 'Derf',
     content: 'Lets make Fresh-New-Text!',
+    color: ''
   },
   {
     id: 2,
     author: 'Ferf',
     content: 'mmmmmm Ok!',
+    color: 'success'
   },
   {
     id: 3,
     author: 'Ferg',
     content: 'I hate Yeezys',
-  }
+    color: 'primary'
+  },
 ]
 
-app.get('/', (req, res) => res.send(messages))
+// const currentMessages = [...messages]
+
+let currentId = 3;
+const genId = () => ++currentId;
+
+const colors = ['', 'dark', 'primary', 'info', 'success', 'warning', 'danger']
+
+const randomColor = () => {
+  return colors[Math.floor(Math.random()*colors.length)];
+}
+
+app.get('/api/messages', (req, res) => {
+  console.log('GET received')
+  res.send(messages)
+  console.log('Messages Sent or something part, dammit!!!!')
+})
+
+app.post('/api/messages', (req, res) => {
+
+  let id = genId();
+  let author = req.body.author;
+  let content = req.body.content;
+  let color = randomColor();
+
+  messages.unshift({ id, author, content, color });
+    res.send(messages);
+    console.log('Messages Sent or something part, dammit!!!!')
+  })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
